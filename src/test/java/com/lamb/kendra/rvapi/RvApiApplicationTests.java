75af62d7ecb4128648c0154e3f8f75cf2fba8489
klambo94@gmail.com <klambo94@gmail.com>
2018-08-22 15:03:45 -0500

    Inital Commit

---
+++ src/test/java/com/lamb/kendra/rvapi/RvApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lamb.kendra.rvapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RvApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
