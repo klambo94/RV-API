7baf1e7aadc0df73001a09ce4629ade0e36ae735
klambo94@gmail.com <klambo94@gmail.com>
2018-08-22 15:14:01 -0500

    RV-1: Intial commit for Move Checklist endpoints

---
+++ src/main/java/com/lamb/kendra/rvapi/MoveCheckList/MoveCheckListController.java
@@ -0,0 +1,36 @@
+package com.lamb.kendra.rvapi.MoveCheckList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class MoveCheckListController {
+
+    @Autowired
+    private MoveCheckListService moveCheckListService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/moveLocations")
+    public List<Item> getCheckList() {
+        return moveCheckListService.getAllItems();
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "/moveLocations")
+    public void addItemToList(@RequestBody Item item) {
+        moveCheckListService.addItem(item);
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "/moveLocations")
+    public void updateItem(@RequestBody Item item) {
+        moveCheckListService.updateItem(item);
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/moveLocations")
+    public void deleteItem(@RequestBody String name) {
+        moveCheckListService.deleteItem(name);
+    }
+}
