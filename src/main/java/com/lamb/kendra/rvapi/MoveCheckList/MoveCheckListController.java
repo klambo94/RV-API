1f8dace973e048b5304c5a4f25ee62d8b8cdfa5c
klambo94@gmail.com <klambo94@gmail.com>
2018-08-23 23:12:28 -0500

    RV-2: Basic functionality for moveLocations endpoint is complete with get, add, update, and delete UI.

--- src/main/java/com/lamb/kendra/rvapi/MoveCheckList/MoveCheckListController.java
+++ src/main/java/com/lamb/kendra/rvapi/MoveCheckList/MoveCheckListController.java
@@ -1,12 +1,15 @@
 package com.lamb.kendra.rvapi.MoveCheckList;
 
+import io.swagger.models.Model;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.validator.constraints.pl.REGON;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,24 +21,42 @@ public class MoveCheckListController {
 
     @RequestMapping(method = RequestMethod.GET, value = "/moveLocations")
     public ModelAndView getCheckList() {
+        List<Item> items = new ArrayList<>();
+        moveCheckListService.getAllItems().forEach(items::add);
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("moveLocations");
-        modelAndView.addObject("items", moveCheckListService.getAllItems());
+        modelAndView.addObject("items", items);
         return modelAndView;
     }
 
-    @RequestMapping(method = RequestMethod.POST, value = "/moveLocations")
-    public void addItemToList(@RequestBody Item item) {
+    @RequestMapping(method = RequestMethod.POST, value = "/moveLocations/addItem")
+    public ModelAndView addItemToList(@RequestParam String name, @RequestParam String description) {
+
+        ModelAndView modelAndView = new ModelAndView();
+        Item item = new Item();
+        item.setName(name);
+        item.setDescription(description);
         moveCheckListService.addItem(item);
+        modelAndView.setViewName("redirect:/moveLocations");
+
+        return modelAndView;
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/moveLocations")
-    public void updateItem(@RequestBody Item item) {
+    @RequestMapping(method = RequestMethod.POST, value = "/moveLocations/editItem")
+    public ModelAndView editItem(@RequestBody Item item, @RequestParam String name, @RequestParam String description) {
         moveCheckListService.updateItem(item);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/moveLocations");
+
+        return modelAndView;
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/moveLocations")
-    public void deleteItem(@RequestBody String name) {
+    @RequestMapping(method = RequestMethod.POST, value = "/moveLocations/deleteItem")
+    public ModelAndView deleteItem(@RequestParam String name) {
         moveCheckListService.deleteItem(name);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("redirect:/moveLocations");
+        return modelAndView;
     }
 }
